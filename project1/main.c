f54638e48035f898d4d7522997b253f59b935bd2
Brabim <buelasto01@gmail.com>
2024-08-26 19:18:02 +0545

    Initial Commit

---
+++ project1/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  project1
+//
+//  Created by Brabim Adhikari on 26/08/2024.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
